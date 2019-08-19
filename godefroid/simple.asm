a4dcc3d1da06f684416b76a3b1ad138bfa5ac1ea
Artem Dinaburg <artem@traiofbits.com>
2019-08-19 16:43:28 -0400

    A simple assembly file to build to elf

---
+++ godefroid/simple.asm
@@ -0,0 +1,16 @@
+BITS 32
+SECTION .text
+
+global main
+
+main:
+push   ebp
+mov    ebp,esp
+push   ecx
+mov    eax, dword [ebp+0x8]
+mov    cl, byte [eax]
+mov    byte [ebp-0x1], cl
+mov    esp,ebp
+pop    ebp
+ret    0x0
+
