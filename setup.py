662bcbf46d3f15887c7c9b1cfd160b149394b056
pasztorpisti <pasztorpisti@gmail.com>
2020-02-06 01:10:46 +0000

    Fix the output of './setup.py sdist'

--- setup.py
+++ setup.py
@@ -2,28 +2,20 @@
 # Copyright 2019 Trail of Bits, all rights reserved.
 
 import setuptools
-import os
 import sys
 
 if sys.version_info < (3, 5):
     print("Microx is only supported on Python 3.5 and above.")
     exit(1)
 
-MICROX_DIR = os.path.dirname(os.path.realpath(__file__))
-os.chdir(MICROX_DIR)
-
 microx_core = setuptools.Extension(
     "microx_core",
-    include_dirs=[
-        MICROX_DIR,
-        os.path.join(MICROX_DIR, "third_party", "include")],
-    sources=[
-        os.path.join(MICROX_DIR, "microx", "Executor.cpp"),
-        os.path.join(MICROX_DIR, "microx", "Python.cpp")],
+    include_dirs=[".", "third_party/include"],
+    sources=["microx/Executor.cpp", "microx/Python.cpp"],
     extra_compile_args=["-std=gnu++11", "-g3", "-O0"],
     libraries=["xed"],
-    library_dirs=[os.path.join(MICROX_DIR, "third_party", "lib")],
-    runtime_library_dirs=[os.path.join(MICROX_DIR, "third_party", "lib")])
+    library_dirs=["third_party/lib"],
+    runtime_library_dirs=["third_party/lib"])
 
 setuptools.setup(
     name="microx",
