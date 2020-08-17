ad22232bd57e726378f76751f88f2c2a608fe8ce
William Woodruff <william@trailofbits.com>
2020-08-17 16:42:10 -0400

    treewide: Prepare for automatic PyPI publishes

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # Copyright 2019 Trail of Bits, all rights reserved.
 
+import os
 import setuptools
 import sys
 
 if sys.version_info < (3, 5):
     print("Microx is only supported on Python 3.5 and above.")
     exit(1)
+
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, "VERSION")) as f:
+    VERSION = f.read().strip()
 
 microx_core = setuptools.Extension(
     "microx_core",
@@ -20,7 +26,7 @@ microx_core = setuptools.Extension(
 
 setuptools.setup(
     name="microx",
-    version="1.0",
+    version=VERSION,
     description="x86 and x86_64 micro-executor.",
     author="Peter Goodman",
     author_email="peter@trailofbits.com",
